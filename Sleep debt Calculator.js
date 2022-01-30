3f82e9acad11b4b37e3b6f94a870aa5a3450ffb5
Ethan <92686703+Ethan-Master-Coding@users.noreply.github.com>
2022-01-30 15:06:09 -0500

    Added formating to the JS

--- Sleep debt Calculator.js
+++ Sleep debt Calculator.js
@@ -2,46 +2,55 @@ function getSleepHours(day) {
     day = day.toLowerCase();
   
     if (day == "sunday") {
-      return 6
+      return 6;
     } else if (day == "monday") {
-      return 7
+      return 7;
     } else if (day == "tuesday") {
-      return 7
+      return 7;
     } else if (day == "wednesday") {
-      return 6
+      return 6;
     } else if ((day = "thursday")) {
-      return 7
+      return 7;
     } else if (day == "friday") {
-      return 7
+      return 7;
     } else if (day == "saterday") {
-      return 6
+      return 6;
     } else {
       return "Please enter a vailid day.";
     }
   }
-
-function getActualSleepHours() {
-  actualSleepTimePerWeek = getSleepHours("sunday") + getSleepHours("monday") + getSleepHours("Tuesday") + getSleepHours("WeDNeSdaY") + getSleepHours("Thursday") + getSleepHours("Friday") + getSleepHours("Saterday")
-  return actualSleepTimePerWeek
-
   
-}
-
-function getIdealSleepHours() {
-  idealHours = 8
-  return idealHours * 7
-}
-
-function calculateSleepDebt() {
-  actualSleepHours = getActualSleepHours()
-  idealSleepHours = getIdealSleepHours()
-  ifNotEqual = actualSleepHours - idealSleepHours
-
-  if (actualSleepHours == idealSleepHours) {
-      console.log("You got the perfict amonth of sleep!")
-  }else if (actualSleepHours > idealSleepHours) {
-      console.log(`You got ${ifNotEqual} more sleep then needed!`)
-  }else{
-      console.log("You did not get enought sleep. Try sleeping " + ifNotEqual * -1 + " more.")
+  function getActualSleepHours() {
+    actualSleepTimePerWeek =
+      getSleepHours("sunday") +
+      getSleepHours("monday") +
+      getSleepHours("Tuesday") +
+      getSleepHours("WeDNeSdaY") +
+      getSleepHours("Thursday") +
+      getSleepHours("Friday") +
+      getSleepHours("Saterday");
+    return actualSleepTimePerWeek;
   }
-}
+  
+  function getIdealSleepHours() {
+    idealHours = 8;
+    return idealHours * 7;
+  }
+  
+  function calculateSleepDebt() {
+    actualSleepHours = getActualSleepHours();
+    idealSleepHours = getIdealSleepHours();
+    ifNotEqual = actualSleepHours - idealSleepHours;
+  
+    if (actualSleepHours == idealSleepHours) {
+      console.log("You got the perfict amonth of sleep!");
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log(`You got ${ifNotEqual} more sleep then needed!`);
+    } else {
+      console.log(
+        "You did not get enought sleep. Try sleeping " +
+          ifNotEqual * -1 +
+          " more."
+      );
+    }
+  }
